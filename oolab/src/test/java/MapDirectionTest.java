f8d8ac926a16166ae7f719d5800833fb718aa196
Łukasz Dragon <lukasz.b.dragon@gmail.com>
2024-10-28 09:42:49 +0100

    Lab 2: Model obiektowy

---
+++ oolab/src/test/java/MapDirectionTest.java
@@ -0,0 +1,31 @@
+import agh.ics.oop.model.MapDirection;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MapDirectionTest {
+    @Test
+    public void next() {
+        MapDirection d = MapDirection.NORTH;
+        d = d.next();
+        assertEquals(MapDirection.EAST, d);
+        d = d.next();
+        assertEquals(MapDirection.SOUTH, d);
+        d = d.next();
+        assertEquals(MapDirection.WEST, d);
+        d = d.next();
+        assertEquals(MapDirection.NORTH, d);
+    }
+
+    @Test
+    public void previous() {
+        MapDirection d = MapDirection.NORTH;
+        d = d.previous();
+        assertEquals(MapDirection.WEST, d);
+        d = d.previous();
+        assertEquals(MapDirection.SOUTH, d);
+        d = d.previous();
+        assertEquals(MapDirection.EAST, d);
+        d = d.previous();
+        assertEquals(MapDirection.NORTH, d);
+    }
+}
