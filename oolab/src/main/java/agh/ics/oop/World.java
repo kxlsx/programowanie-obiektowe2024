9bafbef6565ea88cd92f7ea0832626d53e0622bf
Łukasz Dragon <lukasz.b.dragon@gmail.com>
2024-11-11 17:34:46 +0100

    Lab 4: Interfejsy i mapy

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -4,15 +4,17 @@ import java.util.List;
 
 import agh.ics.oop.model.Animal;
 import agh.ics.oop.model.MoveDirection;
+import agh.ics.oop.model.RectangularMap;
 import agh.ics.oop.model.Vector2d;
 
 public class World {
-    public static final Vector2d LOWER_LEFT = new Vector2d(0, 0);
-    public static final Vector2d UPPER_RIGHT = new Vector2d(4, 4);
+    public final static int WIDTH = 4;
+    public final static int HEIGHT = 4;
 
     public static void main(String[] args) {
         List<MoveDirection> directions = OptionsParser.parse(args);
         List<Vector2d> positions = List.of(new Vector2d(2,2), new Vector2d(3,4));
-        Simulation simulation = new Simulation(positions, directions); simulation.run();
+        Simulation simulation = new Simulation(positions, directions, new RectangularMap(WIDTH, HEIGHT));
+        simulation.run();
     }
 }
